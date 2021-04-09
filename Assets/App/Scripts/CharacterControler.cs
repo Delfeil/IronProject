8031c8f453e7bc540c3832fdf34ddbfb9cc05dbe
Delfeil <delfeilcasanova@gmail.com>
2021-04-10 00:20:53 +0200

    [feature] Character movments

---
+++ Assets/App/Scripts/CharacterControler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControler : MonoBehaviour
+{
+    [Header("params")]
+    [SerializeField] float moveFactor;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Move(MovmentType moveType)
+    {
+        Vector3 dir;
+        switch (moveType)
+        {
+            case MovmentType.UP:
+                dir = Vector3.up;
+                break;
+            case MovmentType.DOWN:
+                dir = Vector3.down;
+                break;
+            case MovmentType.LEFT:
+                dir = Vector3.left;
+                break;
+            case MovmentType.RIGHT:
+                dir = Vector3.right;
+                break;
+            default:
+                return;
+                break;
+        }
+        transform.position += dir * moveFactor;
+    }
+}
