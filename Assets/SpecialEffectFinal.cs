2b284c6be0c2d520db88ad6cefe94e921cf01341
Paulux92500 <paul.ray92500@gmail.com>
2021-04-11 04:58:48 +0200

    Final portal

---
+++ Assets/SpecialEffectFinal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialEffectFinal : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(new Vector3(0f, 0f, 7f * Time.deltaTime), Space.World);
+    }
+}
